b673206d002913cdb782deb3adf85266e110a850
Nick <nikmaunt@gmail.com>
2023-04-03 22:55:19 +0300

    Updates

---
+++ precache-manifest.67058ba417cf6f082adaa63f4af8d226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc00451a69387642bd034fc81281c53e",
+    "url": "/TodoListApp/index.html"
+  },
+  {
+    "revision": "81e641bb5edc7f65fef7",
+    "url": "/TodoListApp/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "75eebd45f5c8c867397d",
+    "url": "/TodoListApp/static/js/2.fd50df5c.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoListApp/static/js/2.fd50df5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e641bb5edc7f65fef7",
+    "url": "/TodoListApp/static/js/main.64167b11.chunk.js"
+  },
+  {
+    "revision": "fba7af60763838290c31",
+    "url": "/TodoListApp/static/js/runtime-main.557e3bd5.js"
+  }
+]);
