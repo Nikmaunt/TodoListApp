8923a58b8ffe9d967b7f8d75c1c9d1b62c74ef63
Nick <nikmaunt@gmail.com>
2023-04-03 22:54:33 +0300

    gh-pages added

---
+++ src/utils/redux-utils.ts
@@ -0,0 +1,18 @@
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux'
+import {AppDispatchType, RootReducerType} from './types'
+import {ActionCreatorsMapObject, bindActionCreators} from 'redux'
+import {useMemo} from 'react'
+
+export const useAppDispatch = () => useDispatch<AppDispatchType>()
+
+export function useActions<T extends ActionCreatorsMapObject<any>>(actions: T) {
+    const dispatch = useAppDispatch()
+
+    const boundActions = useMemo(() => {
+        return bindActionCreators(actions, dispatch)
+    }, [])
+
+    return boundActions
+}
+
+export const useAppSelector: TypedUseSelectorHook<any> = useSelector;
